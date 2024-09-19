625155afe802797508e015c86d214b38333726d5
sub_desktop <y24m019@mail.ryukoku.ac.jp>
2024-09-19 18:36:57 +0900

    subPC 4ch_10cm recoding

--- rec_signal_noise.py
+++ rec_signal_noise.py
@@ -261,8 +261,8 @@ def recoding2(wave_files, out_dir, snr, reverbe_sec, reverbe_par, channel=1, is_
     mic_center = room_dim / 2  # アレイマイクの中心[x,y,z](m)
     num_channels = channel  # マイクの個数(チャンネル数)
     distance = 0.1  # 各マイクの間隔(m)
-    mic_coordinate = rec_util.set_mic_coordinate(center=mic_center, num_channels=num_channels, distance=distance)  # 各マイクの座標
-    # mic_coordinate = rec_util.set_circular_mic_coordinate(center=mic_center, num_channels=num_channels, radius=distance)
+    mic_coordinate = rec_util.set_mic_coordinate(center=mic_center, num_channels=num_channels, distance=distance)  # 線形アレイの場合
+    # mic_coordinate = rec_util.set_circular_mic_coordinate(center=mic_center, num_channels=num_channels, radius=distance)  # 円形アレイの場合
 
     doas = np.array([
         [np.pi/2., np.pi/2],
@@ -364,9 +364,9 @@ def process_recoding_thread(angle, angle_name):
     noise_path = f'{const.SAMPLE_DATA_DIR}\\noise\\{noise_type}.wav'  # 雑音信号のディレクトリ
     snr = 10  # SNR
     reverbe_sec = 0.5  # 残響
-    ch = 2  # マイク数
+    ch = 4  # マイク数
     is_split = False  # 信号の保存方法 True:各チャンネルごとにファイルを分ける False:1つのファイルにまとめる
-    out_dir = f"{const.MIX_DATA_DIR}\\{speech_type}_{noise_type}_{snr:02}{snr:02}dB_{int(reverbe_sec * 10):02}sec_{ch}ch_circular_10cm\\{angle_name}"
+    out_dir = f"{const.MIX_DATA_DIR}\\{speech_type}_{noise_type}_{snr:02}{snr:02}dB_{int(reverbe_sec * 10):02}sec_{ch}ch_10cm\\{angle_name}"
     print('out_dir', out_dir)
 
 
