8cdb23069262370332f0921c5c497199f2ada3af
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2025-01-25 22:13:42 +0900

    mainPC 3次元の表示を可能にする

--- plot_room.py
+++ plot_room.py
@@ -32,17 +32,17 @@ def set_circular_mic_coordinate(center, num_channels, radius):
     return coordinate
 
 
-def plot_room(channel=1, distance=3):
+def plot_room(channel=1, distance=3, array_type="liner"):
     """ シミュレーションのパラメータ """
-    room_dim = np.r_[10.0, 10.0, 10.0]  # 部屋の大きさ[x,y,z](m)
+    room_dim = np.r_[3.0, 3.0, 3.0]  # 部屋の大きさ[x,y,z](m)
     mic_center = room_dim / 2  # アレイマイクの中心[x,y,z](m)
     num_channels = channel  # マイクの個数(チャンネル数)
-    # distance = distance*0.01  # 各マイクの間隔(m)   線形
-    # mic_coordinate = rec_util.set_mic_coordinate(center=mic_center,
-    #                                              num_channels=num_channels,
-    #                                              distance=distance*0.01)  # 線形 マイクの座標
-    # distance = distance*0.01/2  # 各マイクの間隔(m) 円形
-    mic_coordinate = rec_util.set_circular_mic_coordinate(center=room_dim/2, num_channels=num_channels, radius=distance*0.01/2)   # 円形 マイクの座標
+    if array_type == "liner":
+        mic_coordinate = rec_util.set_mic_coordinate(center=mic_center,
+                                                     num_channels=num_channels,
+                                                     distance=distance*0.01)  # 線形 マイクの座標
+    else :
+        mic_coordinate = rec_util.set_circular_mic_coordinate(center=room_dim/2, num_channels=num_channels, radius=distance*0.01/2)   # 円形 マイクの座標
     doas = np.array([
         [np.pi / 2., np.pi / 2.],  # 話者(音源1)
         [np.pi / 2., np.pi * 0 / 4],  # 雑音(音源2)
@@ -52,7 +52,7 @@ def plot_room(channel=1, distance=3):
         [np.pi / 2., np.pi * 4 / 4],  # 雑音(音源6)
 
     ])  # 音源の方向[仰角, 方位角](ラジアン)
-    distance = [2., 3., 3., 3., 3., 3.]  # 音源とマイクの距離(m)
+    distance = [0.5, 0.7, 0.7, 0.7, 0.7, 0.7]  # 音源とマイクの距離(m)
     """ 各音源の座標 """
     source_coordinate = rec_util.set_souces_coordinate2(doas, distance, mic_center)
 
@@ -71,37 +71,100 @@ def plot_room(channel=1, distance=3):
     plt.legend(loc="best", markerscale=0.75)
     plt.show()
 
-    """ 部屋の出力 """
-    # fig, ax = room_mix.plot()
-    # ax.set_xlim([0, 10])
-    # ax.set_ylim([0, 10])
-    # ax.set_zlim([0, 10])
-    # plt.show()
+
+
+def plot_room_3D(room_dimensions, microphones, sound_sources):
+    """
+    部屋を3D散布図としてプロットする関数
+
+    Parameters:
+        room_dimensions: tuple (x, y, z) 部屋のサイズ
+        microphones: list of tuples 各マイクの座標 [(x1, y1, z1), ...]
+        sound_sources: list of tuples 各音源の座標 [(x1, y1, z1), ...]
+    """
+    # 部屋のサイズ
+    x_dim, y_dim, z_dim = room_dimensions
+
+    # 3Dプロットのセットアップ
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # 部屋の範囲を設定
+    ax.set_xlim([1., 2.5])
+    ax.set_ylim([1., 2.5])
+    ax.set_zlim([1, 2])
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    # マイクをプロット (青)
+    for idx, (mic_x, mic_y, mic_z) in enumerate(microphones):
+        if idx == 0:
+            ax.scatter(mic_x, mic_y, mic_z, marker="d", c='blue', label="Microphones", s=100)
+        ax.scatter(mic_x, mic_y, mic_z, marker="d", c='blue', s=100)
+
+    # 音源をプロット (赤)
+    src_x, src_y, src_z = zip(sound_sources[0])  # 座標を分解
+    ax.scatter(src_x, src_y, src_z, c='red', label='Speaker', s=100)
+    # 雑音をプロット (緑)
+    src_x, src_y, src_z = zip(sound_sources[1])  # 座標を分解
+    ax.scatter(src_x, src_y, src_z, marker="x", c='green', label='Noise', s=100)
+
+
+    # 凡例を追加
+    ax.legend()
+
+    # グリッドを表示
+    ax.grid(True)
+    # plt.title("3D Room with Microphones and Sound Sources")
+    plt.show()
 
 
 if __name__ == "__main__":
     print("main")
-    # a = np.array([[1., 4.],
-    #               [2., 5.],
-    #               [3., 6.]])
-    # print(a[0, 0], a[1, 0])
-    # print(a[0, 1], a[1, 1])
 
     """録音(シミュレーション)"""
-    plot_room(channel=4, distance=10)
-
-    # ch = 4
-    # radius = 1  # 半径
-    # angle_list = np.array([[360*i/ch]for i in range(ch)])
-    # print(angle_list)
-    # angle_list = np.array([[np.cos(2*np.pi*i/ch) * radius, np.sin(2*np.pi*i/ch) * radius]for i in range(ch)])
-    # print(angle_list)
-    # angle_list = np.array([[np.cos(360*i/ch) * radius, np.sin(360*i/ch) * radius]for i in range(ch)])
-    # print(angle_list)
-    #
-
-
+    # plot_room(channel=4, distance=10)
 
     # Example usage
-    # plot_points_on_circle(center=(5, 5), num_points=4, radius=1)
-    set_circular_mic_coordinate([5, 5, 5], 4, 1)
+    # set_circular_mic_coordinate(center=(1.5, 1.5, 1.5), num_channels=4, radius=4)
+
+    """ マイクに関するパラメータ """
+    num_mic = 4
+    mic_distance = 10    # マイク間隔(cm) 線形：各マイクの間隔, 円形: 円形アレイの直径
+
+    """ 部屋に関するパラメータ """
+    room_dim = np.array([3, 3, 3])  # 部屋のサイズ (x, y, z)
+    angle = 0  # 度 →　ラジアン
+    doas = np.array([
+        [np.pi / 2., np.pi / 2],    # 目的音声の座標
+        [np.pi / 2., np.deg2rad(angle)] # 雑音の座標
+    ])  # 音源の方向[仰角, 方位角](ラジアン)
+    distance = [0.5, 0.7]  # 音源とマイクの距離(m)
+
+    mic_center = room_dim / 2  # アレイマイクの中心[x,y,z](m)
+    mic_coordinate = rec_util.set_mic_coordinate(center=mic_center, num_channels=num_mic, distance=mic_distance*0.01)  # 線形アレイの場合
+    # mic_coordinate = rec_util.set_circular_mic_coordinate(center=mic_center, num_channels=num_channels, radius=distance)  # 円形アレイの場合
+
+    """ 各音源の座標 """
+    source_coordinate = rec_util.set_souces_coordinate2(doas, distance, mic_center)
+
+    # マイクの座標リスト
+    # microphones = [
+    #     (2, 2, 1),
+    #     (8, 2, 1),
+    #     (2, 6, 1),
+    #     (8, 6, 1),
+    # ]
+
+    # 音源の座標リスト
+    # sound_sources = [
+    #     (5, 4, 3),
+    #     (7, 7, 5),
+    # ]
+
+    # プロットを実行
+    print(mic_coordinate.T)
+    print(source_coordinate.T)
+    plot_room_3D(room_dim, mic_coordinate.T, source_coordinate.T)
+
