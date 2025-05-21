4727d85532dd6b6d1d42a00425b6598bacbf90e3
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2025-05-21 15:06:15 +0900

    実験用の部屋に変更

--- sample_IR.py
+++ sample_IR.py
@@ -5,32 +5,35 @@ import numpy as np
 import soundfile as sf
 import matplotlib.pyplot as plt
 import pyroomacoustics as pra
+from mymodule import const, rec_config as rec_conf, rec_utility as rec_util
+
 
 # パラメタ
-fs = 44100       # サンプリング周波数
+fs = 16000       # サンプリング周波数
 absorption = 0.2 # 反射率
 max_order = 10    # 次数
 
 # 3次元の部屋形状生成
-corners = np.array([[0,0], [0,3], [5,3], [5,1], [3,1], [3,0]]).T
-room = pra.Room.from_corners(
-        corners,
-        max_order=max_order,
-        fs=fs,
-        absorption=absorption
-    )
-room.extrude(2.) # 高さを設定
+room_dim = np.r_[3.0, 3.0, 3.0]  # 部屋の大きさ[x,y,z](m)
+room = pra.ShoeBox(room_dim, fs=fs, max_order=max_order, absorption=absorption)  # 残響のみ
 
 # 音源位置を設定
-room.add_source([1., 1., 1.])
+doas = np.array([
+    [np.pi / 2., np.pi / 2],
+    ])  # 音源の方向[仰角, 方位角](ラジアン)
+distance = [0.5, 0.7]  # 音源とマイクの距離(m)
+mic_coordinate = rec_util.set_mic_coordinate(center=room_dim / 2, num_channels=1,
+                                                     distance=0)  # 線形アレイの場合
+source_coordinate = rec_util.set_souces_coordinate2(doas, distance, room_dim / 2)
+room.add_source(source_coordinate)
 # マイク位置を設定
-room.add_microphone([1., 0.7, 1.2])
+room.add_microphone_array(pra.MicrophoneArray(mic_coordinate, fs=room.fs))
 
 # 部屋形状、音源・マイク位置プロット
 fig, ax = room.plot()
-ax.set_xlim([0, 5])
+ax.set_xlim([0, 3])
 ax.set_ylim([0, 3])
-ax.set_zlim([0, 2])
+ax.set_zlim([0, 3])
 plt.show()
 
 
