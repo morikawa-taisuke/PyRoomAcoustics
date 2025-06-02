6f6b6de5c9f4afbcfd4f1b590e79219decb87e84
sub_desktop <y24m019@mail.ryukoku.ac.jp>
2025-06-02 23:00:33 +0900

    実行文の変更

--- Generate_conditions.py
+++ Generate_conditions.py
@@ -31,8 +31,9 @@ def main(wav_dir, noise_dir, csv_path):
         print(f"CSV file saved to {csv_path}")
 
 if __name__ == "__main__":
+    test_train = "train"
     for i in range(1, 5+1):
-        wav_dir = os.path.join(const.SAMPLE_DATA_DIR, "speech", "subset_DEMAND", "test")
+        wav_dir = os.path.join(const.SAMPLE_DATA_DIR, "speech", "subset_DEMAND", test_train)
         noise_dir = os.path.join(const.SAMPLE_DATA_DIR, "noise", "DEMAND")
-        csv_path = os.path.join(const.SAMPLE_DATA_DIR, "speech", "subset_DEMAND", "condition", "test", f"condition_{i}.csv")
+        csv_path = os.path.join(const.SAMPLE_DATA_DIR, "speech", "GNN", "subset_DEMAND", "condition", test_train, f"condition_{i}.csv")
         main(wav_dir, noise_dir, csv_path)
