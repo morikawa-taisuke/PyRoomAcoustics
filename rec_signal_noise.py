2dcf6366768a41d5a402aa25429119becefd9c00
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2025-08-27 18:33:03 +0900

    部屋サイズを5x5x5に変更し、`create_dataset.py`を新規追加。`const.py`のパス定義を修正。

--- rec_signal_noise.py
+++ rec_signal_noise.py
@@ -16,7 +16,7 @@ from mymodule import my_func
 def serch_reverbe_sec(reverbe_sec, channel=1, angle=np.pi):
     reverbe = reverbe_sec
     cnt = 0
-    room_dim = np.r_[3.0, 3.0, 3.0]
+    room_dim = np.r_[5.0, 5.0, 5.0]
     """ 音源の読み込み """
     target_data = rec_util.load_wave_data(f"./mymodule/JA01F049.wav")
     noise_data = target_data
@@ -257,7 +257,7 @@ def recoding2(wave_files, out_dir, snr, reverbe_sec, reverbe_par, channel=1, dis
     """ 音源のパラメータ """
     sample_rate = rec_conf.sampling_rate  # サンプリング周波数
     """ シミュレーションのパラメータ """
-    room_dim = np.r_[3.0, 3.0, 3.0]  # 部屋の大きさ[x,y,z](m)
+    room_dim = np.r_[5.0, 5.0, 5.0]  # 部屋の大きさ[x,y,z](m)
     num_sources = len(wave_data)  # シミュレーションで用いる音源数
     mic_center = room_dim / 2  # アレイマイクの中心[x,y,z](m)
     num_channels = channel  # マイクの個数(チャンネル数)
@@ -422,83 +422,86 @@ def process_recoding_thread(angle, angle_name, reverbe_sec=5):
 
 
 if __name__ == "__main__":
-    print("main")
-    """ シミュレーションの設定"""
-    # angle_list = [np.pi*i/4. for i in range(5)]
-    # angle_name_list = ["Right", "FrontRight", "Front", "FrontLeft", "Left"] # "Right", "FrontRight", "Front", "FrontLeft", "Left"
-    angle_list = [math.radians(i) for i in [30, 60, ]]
-    angle_name_list = ["30dig", "60dig", ]  # "Right", "FrontRight", "Front", "FrontLeft", "Left"
-    print(angle_list)
-    # # for channel in channel_list:
-    # # for reverbe_sec in reverbe_list:
-    # reverbe_sec = reverbe_list[0]
-    # out_dir = f"{const.MIX_DATA_DIR}\\{speech_type}_{noise_type}_{snr:02}{snr:02}dB_{int(reverbe_sec * 10):02}sec_{channel_list}ch_circular_10cm\\{angle_name}"
-    start = time.time()
-    """ マルチプロセスの場合 """
-    # with ProcessPoolExecutor() as executor:
-    #     executor.map(process_recoding_thread,
-    #                  angle_list,
-    #                  angle_name_list,)
-    # for reverbe in range(1, 6):
-    # reverbe = 5
-    #
-    # with ProcessPoolExecutor() as executor:
-    #     executor.map(process_recoding_thread,
-    #                  angle_list,
-    #                  angle_name_list,
-    #                  [reverbe]*len(angle_list))
+	print("main")
+	""" シミュレーションの設定"""
+	# angle_list = [np.pi*i/4. for i in range(5)]
+	# angle_name_list = ["Right", "FrontRight", "Front", "FrontLeft", "Left"] # "Right", "FrontRight", "Front", "FrontLeft", "Left"
+	angle_list = [math.radians(i) for i in [30, 60, ]]
+	angle_name_list = ["30dig", "60dig", ]  # "Right", "FrontRight", "Front", "FrontLeft", "Left"
+	print(angle_list)
+	# # for channel in channel_list:
+	# # for reverbe_sec in reverbe_list:
+	# reverbe_sec = reverbe_list[0]
+	# out_dir = f"{const.MIX_DATA_DIR}\\{speech_type}_{noise_type}_{snr:02}{snr:02}dB_{int(reverbe_sec * 10):02}sec_{channel_list}ch_circular_10cm\\{angle_name}"
+	start = time.time()
+	""" マルチプロセスの場合 """
+	# with ProcessPoolExecutor() as executor:
+	#     executor.map(process_recoding_thread,
+	#                  angle_list,
+	#                  angle_name_list,)
+	# for reverbe in range(1, 6):
+	# reverbe = 5
+	#
+	# with ProcessPoolExecutor() as executor:
+	#     executor.map(process_recoding_thread,
+	#                  angle_list,
+	#                  angle_name_list,
+	#                  [reverbe]*len(angle_list))
 
-    # for reverbe in range(1, 6):
-    speech_type = "subset_DEMAND"
-    noise_type = "hoth"
-    # target_dir = "F:\\sound_data\\sample_data\\speech\\DEMAND"  # 目的信号のディレクトリ
-    target_dir = f"{const.SAMPLE_DATA_DIR}\\speech\\{speech_type}\\"  # 目的信号のディレクトリ
-    sub_dir_list = my_func.get_subdir_list(target_dir)
-    noise_path = f"{const.SAMPLE_DATA_DIR}\\noise\\{noise_type}.wav"  # 雑音信号のディレクトリ
-    snr = 10  # SNR [dB]
-    # reverbe = 5  # 残響 [sec]
-    ch = 1  # マイク数 [ch]
-    distance = 0   # マイクの間隔 [cm]
-    # for reverbe in range(1, 5+1):
-    #for angle, angle_name in zip(angle_list, angle_name_list):
-    reverbe = 5
-    # angle_name = "00dig"
-    angle = math.radians(0)
-    out_dir = f"{const.MIX_DATA_DIR}\\{speech_type}_{noise_type}_{snr:02}{snr:02}dB_{reverbe:02}sec_{ch}ch_{distance}cm\\"
-    print("out_dir", out_dir)
+	# for reverbe in range(1, 6):
+	speech_type = "speeker_DEMAND"
+	noise_type = "hoth"
+	# target_dir = "F:\\sound_data\\sample_data\\speech\\DEMAND"  # 目的信号のディレクトリ
+	target_dir = f"{const.SAMPLE_DATA_DIR}/speech/{speech_type}/val"  # 目的信号のディレクトリ
+	sub_dir_list = my_func.get_subdir_list(target_dir)
+	print(sub_dir_list)
+	noise_path = f"{const.SAMPLE_DATA_DIR}/noise/{noise_type}.wav"  # 雑音信号のディレクトリ
+	snr = 10  # SNR [dB]
+	# reverbe = 5  # 残響 [sec]
+	ch = 1  # マイク数 [ch]
+	distance = 0   # マイクの間隔 [cm]
+	# for reverbe in range(1, 5+1):
+	#for angle, angle_name in zip(angle_list, angle_name_list):
+	reverbe = 5
+	# angle_name = "00dig"
+	angle = math.radians(0)
+	out_dir = f"{const.MIX_DATA_DIR}\\{speech_type}_{noise_type}_{snr:02}{snr:02}dB_{reverbe:02}sec_{ch}ch_{distance}cm\\"
+	print("out_dir", out_dir)
 
-    """録音(シミュレーション)"""
-    reverbe_par_json = f"{const.MIX_DATA_DIR}\\reverbe_condition\\{reverbe:02}sec_{ch}ch_{distance}cm.json"
-    if not os.path.isfile(reverbe_par_json):
-        reverbe_par = serch_reverbe_sec(reverbe_sec=reverbe*0.1, channel=ch)  # 任意の残響になるようなパラメータを求める
-        json_data = {"reverbe_par": reverbe_par}
-        """ 出力先のディレクトリの確認 """
-        my_func.exists_dir(my_func.get_dirname(reverbe_par_json))
-        with open(reverbe_par_json, "w") as json_file:
-            json.dump(json_data, json_file, indent=4)
-    else:
-        print("json_path:", reverbe_par_json)
-        with open(reverbe_par_json, "r") as json_file:
-            json_data = json.load(json_file)
-            reverbe_par = json_data["reverbe_par"]
-        # print("b")
+	"""録音(シミュレーション)"""
+	# reverbe_par_json = f"{const.MIX_DATA_DIR}\\reverbe_condition\\{reverbe:02}sec_{ch}ch_{distance}cm.json"
+	# if not os.path.isfile(reverbe_par_json):
+	#     reverbe_par = serch_reverbe_sec(reverbe_sec=reverbe*0.1, channel=ch)  # 任意の残響になるようなパラメータを求める
+	#     json_data = {"reverbe_par": reverbe_par}
+	#     """ 出力先のディレクトリの確認 """
+	#     my_func.exists_dir(my_func.get_dirname(reverbe_par_json))
+	#     with open(reverbe_par_json, "w") as json_file:
+	#         json.dump(json_data, json_file, indent=4)
+	# else:
+	#     print("json_path:", reverbe_par_json)
+	#     with open(reverbe_par_json, "r") as json_file:
+	#         json_data = json.load(json_file)
+	#         reverbe_par = json_data["reverbe_par"]
+	# #     # print("b")
 
-    for sub_dir in sub_dir_list:
-        """音声ファイルリストの作成"""
-        target_list = my_func.get_file_list(os.path.join(target_dir, sub_dir))
-        print(f"len:{len(target_list)}")
-        for target_file in tqdm(target_list):
-            wave_file = []
-            wave_file.append(target_file)
-            wave_file.append(noise_path)
+	reverbe_par = serch_reverbe_sec(reverbe_sec=reverbe * 0.1, channel=ch)  # 任意の残響になるようなパラメータを求める
+	for sub_dir in sub_dir_list:
+		"""音声ファイルリストの作成"""
+		target_list = my_func.get_file_list(os.path.join(target_dir, sub_dir))
+		# target_list = ["/Users/a/Documents/python/PyRoomAcoustics/mymodule/p257_433.wav"]
+		print(f"len:{len(target_list)}")
+		for target_file in tqdm(target_list):
+			wave_file = []
+			wave_file.append(target_file)
+			wave_file.append(noise_path)
 
-            """録音(シミュレーション)"""
-            recoding2(wave_files=wave_file,
-                      out_dir=os.path.join(out_dir, sub_dir),
-                      snr=snr,
-                      reverbe_sec=reverbe*0.1,
-                      reverbe_par=reverbe_par,
-                      channel=ch,
-                      angle=angle)
-    end = time.time()
-    print(f"time:{(end - start) / 60:.2f}min")
+			"""録音(シミュレーション)"""
+			recoding2(wave_files=wave_file,
+					  out_dir=f"{const.MIX_DATA_DIR}/DEMAND_hoth_05dB_500msec/val",
+					  snr=snr,
+					  reverbe_sec=reverbe*0.1,
+					  reverbe_par=reverbe_par,
+					  channel=ch,
+					  angle=angle)
+		end = time.time()
+		print(f"time:{(end - start) / 60:.2f}min")
