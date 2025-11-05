852636565565c1e886ed6e524449d4cb00b97c75
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2025-11-06 02:59:13 +0900

    コミット

--- mymodule/config.py
+++ mymodule/config.py
@@ -10,3 +10,13 @@ FREQS=np.arange(0,Nk,1)*SAMPLE_RATE/N #各ビンの周波数
 
 NFFT = 512
 NOVERLAP = 512//16*15
+
+# 部屋のパラメータ
+REVERBE = 5
+ROOM_DIM = np.r_[5.0, 5.0, 5.0]
+
+# 音源のパラメータ
+DOAS = np.array([
+    [np.pi / 2., np.pi / 2],
+    ])
+DISTANCE = [0.5, 0.7]
