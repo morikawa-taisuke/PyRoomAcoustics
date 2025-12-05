3c2ff16e066622b68bd5f89ce989c6b0aa0706d6
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2025-12-05 22:41:38 +0900

    パス定義を`os.path`から`pathlib.Path`に変更し、新しいディレクトリ定義を追加 (SPEECH_DATA_DIR, NOISE_DATA_DIR, PARMS_DATA_DIR).

--- src/mymodule/const.py
+++ src/mymodule/const.py
@@ -1,4 +1,5 @@
-import os
+from pathlib import Path
+
 """
 construction
 設定
@@ -6,6 +7,9 @@ construction
 """
 # SOUND_DATA_DIR = '/Users/a/Documents/sound_data/'
 SOUND_DATA_DIR = 'C:/Users/kataoka-lab/Desktop/sound_data/'
-SAMPLE_DATA_DIR = os.path.join(SOUND_DATA_DIR, 'sample_data')
-MIX_DATA_DIR = os.path.join(SOUND_DATA_DIR, 'mix_data')
+SAMPLE_DATA_DIR = Path(SOUND_DATA_DIR) / 'sample_data'
+SPEECH_DATA_DIR = Path(SAMPLE_DATA_DIR) / 'speech'
+NOISE_DATA_DIR = Path(SAMPLE_DATA_DIR) / 'noise'
+MIX_DATA_DIR = Path(SOUND_DATA_DIR) / 'mix_data'
+PARMS_DATA_DIR = Path(SOUND_DATA_DIR) / 'preconpute_params'
 
