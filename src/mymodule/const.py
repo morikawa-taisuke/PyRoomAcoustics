04ff49a1e0f787a9e03a65b940c59929dadadc18
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2025-12-06 16:34:58 +0900

    サウンドデータディレクトリの設定を更新: デフォルトを`'/Users/a/Documents/sound_data/'`に変更し、以前の設定をコメントアウト.

--- src/mymodule/const.py
+++ src/mymodule/const.py
@@ -5,8 +5,8 @@ construction
 設定
 
 """
-# SOUND_DATA_DIR = '/Users/a/Documents/sound_data/'
-SOUND_DATA_DIR = 'C:/Users/kataoka-lab/Desktop/sound_data/'
+SOUND_DATA_DIR = '/Users/a/Documents/sound_data/'
+# SOUND_DATA_DIR = 'C:/Users/kataoka-lab/Desktop/sound_data/'
 SAMPLE_DATA_DIR = Path(SOUND_DATA_DIR) / 'sample_data'
 SPEECH_DATA_DIR = Path(SAMPLE_DATA_DIR) / 'speech'
 NOISE_DATA_DIR = Path(SAMPLE_DATA_DIR) / 'noise'
