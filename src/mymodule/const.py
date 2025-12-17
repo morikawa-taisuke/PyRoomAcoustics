ecb3df29e3da03b79b67a65e8cb12ffbaade8741
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2025-12-17 20:19:51 +0900

    定数ディレクトリのPath定義を修正しコードを簡素化

--- src/mymodule/const.py
+++ src/mymodule/const.py
@@ -5,11 +5,11 @@ construction
 設定
 
 """
-# SOUND_DATA_DIR = '/Users/a/Documents/sound_data/'
-SOUND_DATA_DIR = 'C:/Users/kataoka-lab/Desktop/sound_data/'
-SAMPLE_DATA_DIR = Path(SOUND_DATA_DIR) / 'sample_data'
-SPEECH_DATA_DIR = Path(SAMPLE_DATA_DIR) / 'speech'
-NOISE_DATA_DIR = Path(SAMPLE_DATA_DIR) / 'noise'
-MIX_DATA_DIR = Path(SOUND_DATA_DIR) / 'mix_data'
-PARMS_DATA_DIR = Path(SOUND_DATA_DIR) / 'preconpute_params'
+# SOUND_DATA_DIR = Path('/Users/a/Documents/sound_data/')
+SOUND_DATA_DIR = Path('C:/Users/kataoka-lab/Desktop/sound_data/')
+SAMPLE_DATA_DIR = SOUND_DATA_DIR / 'sample_data'
+SPEECH_DATA_DIR = SOUND_DATA_DIR / 'speech'
+NOISE_DATA_DIR = SOUND_DATA_DIR / 'noise'
+MIX_DATA_DIR = SOUND_DATA_DIR / 'mix_data'
+PARMS_DATA_DIR = SOUND_DATA_DIR / 'preconpute_params'
 
