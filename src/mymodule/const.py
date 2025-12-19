9621d4b98fffcb753bcb7fe1eee638a2d5404d4a
y200196 <y24m019@mail.ryukoku.ac.jp>
2025-12-19 11:26:02 +0900

    パスを変更

--- src/mymodule/const.py
+++ src/mymodule/const.py
@@ -7,9 +7,11 @@ construction
 """
 # SOUND_DATA_DIR = Path('/Users/a/Documents/sound_data/')
 SOUND_DATA_DIR = Path('C:/Users/kataoka-lab/Desktop/sound_data/')
+
 SAMPLE_DATA_DIR = SOUND_DATA_DIR / 'sample_data'
-SPEECH_DATA_DIR = SOUND_DATA_DIR / 'speech'
-NOISE_DATA_DIR = SOUND_DATA_DIR / 'noise'
+SPEECH_DATA_DIR = SAMPLE_DATA_DIR / 'speech'
+NOISE_DATA_DIR = SAMPLE_DATA_DIR / 'noise'
+
 MIX_DATA_DIR = SOUND_DATA_DIR / 'mix_data'
 PARMS_DATA_DIR = SOUND_DATA_DIR / 'preconpute_params'
 RIR_DIR = SOUND_DATA_DIR / 'RIR'
